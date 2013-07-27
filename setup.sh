347c61f67fe96434a99d169ab9bdd5f631cb62b9
KAWACHI Takashi <kawachi@p-lucky.net>
2013-07-27 21:27:49 +0900

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 set -e
 set -u
 
+url_base="https://raw.github.com/tkawachi/corona-test-runner/develop"
+
 function check_file() {
     file="$1"
     if [ -a "$file" ]; then
@@ -15,3 +17,10 @@ for f in app_main.lua test_main.lua lunatest.lua; do
     check_file $f
 done
 
+if [ -a main.lua ]; then
+    mv main.lua app_main.lua
+fi
+
+for f in main.lua test_main.lua lunatest.lua; do
+    \curl -O ${url_base}/$f
+done
